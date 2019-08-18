a6443a380aacd2d0dee841200bab0edacd896003
Anchan <symphony_ame@foxmail.com>
2019-08-18 14:36:33 +0800

    命令行计时选择菜单

---
+++ 002-计时选择菜单/开始.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module $PSSCRIPTROOT\choice.psm1
+
+$Options = ("Windows 2000", "Windows XP", "Windows Vista")
+$result = Choice "选择你最喜欢的windows版本：" $Options  8 $true
+Write-Host ""
+    # Choice 包含四个参数 $Title——标题, $Options——选项, $CountDown=10 —— 倒计时，默认10秒, $OpenCountDown=$false —— 倒计时是否开启，默认不开启
+    # Choice "选择要测试的项目：" $Options 5 $true
+if ($result.isTimeout) {
+  Write-Host "超时了！！！"
+}
+Write-Host "你最喜欢的windows是： $($Options[$result.choice]) "
+Read-Host "按Enter退出"
+
